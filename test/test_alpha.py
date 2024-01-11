cbff4fe6d62e3100b6786c2cd698010c8760d733
xhaunst <65455914+xhaunst@users.noreply.github.com>
2024-01-11 15:54:09 +0700

    Update test_alpha.py

--- test/test_alpha.py
+++ test/test_alpha.py
@@ -1,5 +1,7 @@
-from src.Area import *
+from src.Volumn import *
 
 def test_one():
-  assert rectangle(5,2) == 10
-  assert triangle(5,2) == 5
+  assert Box(5,2,2) == 20
+  assert Box(2,1,2) == 4
+  assert Pyramid(5,2,2) == (5*2*2)/3
+  assert Pyramid(2,3,4) == (2*3*4)/3
